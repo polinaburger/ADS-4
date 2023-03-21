2b0d7c8e4350721e4c6bfd04de029ef238265f01
Polina Fomicheva <116427492+polinaburger@users.noreply.github.com>
2023-03-21 23:31:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,68 @@
-// Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+int countPairs1(int *arr, int len, int value){
+  int count = 0;
+  for (int i = 0; i < len-1; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
+    }
+    return count;
+  }
+int countPairs2(int *arr, int len, int value){
+  int count = 0;
+  int left = -1;
+  int right = len-1;
+  while(left < right-1) {
+    int middle = (right + left)/2;
+    if (arr[middle] <= value) {
+      left = middle;
+    } else {
+      right = middle;
+    }
+  }
+  len = right- 1;
+  for (i = len; i >= 0; i--;) {
+    for (j = 0; j < i; j++) {
+      if (arr[j]+arr[i] == value) {
+        count++;
+        } else if (arr[j] + arr[i] > value) {
+        break;
+      }
+    }
+  }
+  return count;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
-}
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+
+int countPairs3(int *arr,int len,int value){
+  int count=0;
+  int left=0;
+  int right=len-1;
+  while(left<right-1) {
+    int middle=(right+left)/2;
+    if (arr[middle]<=value) {
+      left=middle;
+    } else {
+      right=middle;
+    }
+  }
+  len=right-1;
+  for(int i=0;i<len;i++) {
+    right=len-1;
+    left=i+1;
+    int coount=0;
+    while (left<right) {
+      int middle=(right+left)/2;
+      if (arr[mid]<(value-arr[i])) {
+        left = mid+1;
+      } else {
+        right=mid;
+      }
+      while (arr[left]==(value-arr[i])) {
+        coount++;
+        left++;
+        }
+      count=coount+count;
+    }
+  }
+  return count;
 }
